e3ff5a50fc31cf94e859d1153024a58f18e81602
CJonesI <cjones@usv.edu>
2026-04-14 14:49:39 -0700

    post office hour update

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -1,11 +1,11 @@
 extends StaticBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-
+@onready var game_manager_2: Node2D = %"Game Manager 2"
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "Player" && body.multi_fruit > 0:
-		body.multi_fruit -= 1
-	$AnimationPlayer.play("open")
-	queue_free()
+	if game_manager_2.score > 0:
+		game_manager_2.score -= 1
+		$AnimationPlayer.play("open")
+		queue_free()
 	
